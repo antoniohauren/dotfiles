60c5aba0fc3cdd65291f1d5bd351c0c5ce9a2955
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-28 16:44:16 -0300

    change lua line style

--- nvim/lua/plugins/lua-line.lua
+++ nvim/lua/plugins/lua-line.lua
@@ -1,10 +1,25 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "catppuccin",
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "catppuccin-mocha",
+        section_separators = "",
+        component_separators = "|",
+      },
+      sections = {
+        lualine_x = {
+          "encoding",
+          {
+            "fileformat",
+            symbols = {
+              unix = "LF",
+              dos = "CRLF",
+            },
+          },
+          "filetype",
+        },
+      },
+    })
+  end,
 }
