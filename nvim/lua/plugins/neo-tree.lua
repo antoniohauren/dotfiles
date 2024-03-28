9674800aa586d8a3524585e8380e4b5bf709653a
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-28 12:56:35 -0300

    nvim initial config

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  name = "neo-tree",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>")
+  end
+}
