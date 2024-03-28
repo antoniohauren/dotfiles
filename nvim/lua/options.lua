5cee1743b14d310594416454cdd9128905bed9fa
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-28 15:48:47 -0300

    add line numbers to config, also add pane nav

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,3 +4,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+vim.wo.number = true
+vim.wo.relativenumber = true
