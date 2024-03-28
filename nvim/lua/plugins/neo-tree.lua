30db76988f8a8ad086aaae3e61a570d8632a9ee4
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-28 15:37:49 -0300

    refact neo-tree commands

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,32 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	name = "neo-tree",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>")
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  name = "neo-tree",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    local config = require("neo-tree")
+    local command = require("neo-tree.command")
+
+    config.setup({
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            command.execute({ action = "close" })
+          end,
+        },
+      },
+    })
+
+    local toggle = function()
+      command.execute({ action = "focus", toggle = true })
+    end
+
+    vim.keymap.set("n", "<C-n>", toggle, {})
+    vim.keymap.set("n", "<leader>fe", toggle, {})
+  end,
 }
