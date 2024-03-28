591aec5d0bfb06d74b8e3ffd3ed07798e23be2dc
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-28 16:44:35 -0300

    refactor git signs

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,19 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    local gs = require("gitsigns")
+
+    gs.setup()
+
+    local preview = function()
+      gs.preview_hunk()
+    end
+
+    local blame_line = function()
+      gs.toggle_current_line_blame()
+    end
+
+    vim.keymap.set("n", "<leader>gp", preview, {})
+    vim.keymap.set("n", "<leader>gl", blame_line, {})
+  end,
+}
